221f5d8af316da31516fa22cd5ec5e14436502fa
Hani <jhe93@naver.com>
2022-09-09 22:46:08 +0900

    Toast - 버튼이 눌렸을 때 문구 띄우기

--- app/src/main/java/com/metacoding/kotlinbasic01_hani/MainActivity.kt
+++ app/src/main/java/com/metacoding/kotlinbasic01_hani/MainActivity.kt
@@ -3,6 +3,7 @@ package com.metacoding.kotlinbasic01_hani
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         clickBtn.setOnClickListener {
+            //큰 버튼이 눌렸을 때 할 일
             //  주석 : 코드에 메모 남기기 ->
             //  ctrl + / : 커서가 있는 줄을 주석 처리
 
             Log.d("메인화면로그", "클릭용 버튼 눌림")
             Log.e("메인화면 로그", "에러 관련 로그")
         }
+
+        smallBtn.setOnClickListener {
+            //작은 버튼이 눌렸을 때 할 일
+            Toast.makeText(this, "작은 버튼 눌림",Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 
 }
