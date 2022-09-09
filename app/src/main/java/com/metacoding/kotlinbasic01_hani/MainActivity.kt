9b3343980629fcd5b6bf3cb996054fe38ee55d06
Hani <jhe93@naver.com>
2022-09-09 22:19:42 +0900

    에러 로그 찍어보기

--- app/src/main/java/com/metacoding/kotlinbasic01_hani/MainActivity.kt
+++ app/src/main/java/com/metacoding/kotlinbasic01_hani/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
             //  ctrl + / : 커서가 있는 줄을 주석 처리
 
             Log.d("메인화면로그", "클릭용 버튼 눌림")
-
+            Log.e("메인화면 로그", "에러 관련 로그")
         }
     }
 
